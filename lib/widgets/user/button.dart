b427298f6379bfd5deb14191db0e821c9196127e
whabl0220 <stargalaxy1579@gmail.com>
2025-07-16 17:58:30 +0900

    feat: "Button 위젯 추가"

---
+++ lib/widgets/user/button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+  final double width;
+  final double height;
+
+  const Button({Key? key, this.text = '텍스트', this.onPressed, this.width = 220, this.height = 48}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: const Color(0xFFD9D9D9),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.35),
+              blurRadius: 20,
+              offset: const Offset(0, 8),
+              spreadRadius: 0,
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
