b216be3a4c0c473945aa2a2e3e6b03f1961031d2
whabl0220 <stargalaxy1579@gmail.com>
2025-07-17 00:33:17 +0900

    feat: "라우팅 파일 제작"

---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'screens/common/title.dart';
+import 'screens/user/home.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+  '/': (context) => const TitleScreen(),
+  '/home': (context) => const HomeScreen(),
+};
