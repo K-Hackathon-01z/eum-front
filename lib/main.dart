b6b683ebb54338cf20719c353e6ed7eb5f966735
keyboardbear <daniel010203@naver.com>
2025-07-22 16:44:50 +0900

    career_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:eum_demo/screens/user/career_test.dart';
+import 'package:eum_demo/screens/common/title.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/user/navigation.dart';
@@ -6,14 +6,7 @@ import 'routes.dart';
 
 // 위젯 파일들은 따로 dart 파일에 두고 import해도 됩니다. 지금은 예시.
 
-// void main() => runApp(const PreviewApp(child: TitleScreen()));
-void main() => runApp(
-  MaterialApp(
-    home: CareerTestScreen(), // ← 여기만 바꿔주면 됨!
-    debugShowCheckedModeBanner: false,
-  ),
-);
-
+void main() => runApp(const PreviewApp(child: TitleScreen()));
 // 보고 싶은 위젯은 아래처럼 child에 직접 전달!
 
 // 전체 앱이 아니라, SINGLE 위젯만 centering/배경 등이 적용된 프리뷰 앱!
