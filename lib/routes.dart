455d3222f036342de4ce21e0d573df14742204e3
whabl0220 <stargalaxy1579@gmail.com>
2025-07-22 18:00:59 +0900

    refactor: "전역적으로 라우팅 경로 관리"

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/common/title.dart';
 import 'screens/user/home.dart';
+import 'screens/user/government_help.dart';
+import 'screens/user/skill_list_screen.dart';
+import 'screens/user/creator_list_screen.dart';
+import 'screens/user/onedayclass.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => const TitleScreen(),
   '/home': (context) => const RootScreen(),
+  '/government-help': (context) => const GovernmentHelpScreen(),
+  '/skill-list': (context) => const SkillListScreen(),
+  '/creator-list': (context) => const CreatorListScreen(),
+  '/oneday-class': (context) => const OnedayClassScreen(),
 };
