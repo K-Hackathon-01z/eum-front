67388d6187a7e6d06566215997ed373ac81cc4a0
keyboardbear <daniel010203@naver.com>
2025-08-05 20:53:56 +0900

    Test: pr 전 테스트 후 ui 부분 수정

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,7 @@ import 'routes.dart';
 // 위젯 파일들은 따로 dart 파일에 두고 import해도 됩니다. 지금은 예시.
 
 void main() => runApp(const PreviewApp(child: TitleScreen()));
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       debugShowCheckedModeBanner: false, //
-//       home: MyInfoPage(), // ← 테스트할 위젯
-//     ),
-//   );
-// }
+
 //보고 싶은 위젯은 아래처럼 child에 직접 전달!
 
 // 전체 앱이 아니라, SINGLE 위젯만 centering/배경 등이 적용된 프리뷰 앱!
