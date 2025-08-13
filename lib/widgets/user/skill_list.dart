db2577636d95b0d13507366a251119d22f2c0bb7
whabl0220 <stargalaxy1579@gmail.com>
2025-08-13 16:08:56 +0900

    refactor: "기술 상세 정보 수정"

--- lib/widgets/user/skill_list.dart
+++ lib/widgets/user/skill_list.dart
@@ -15,7 +15,7 @@ class SkillList extends StatelessWidget {
           () {
             showDialog(
               context: context,
-              builder: (_) => SkillDetail(name: title, skill: '기술 예시', works: '주요작품 예시', bio: '약력 예시'),
+              builder: (_) => SkillDetail(name: title, category: '분류 예시', description: '설명 예시', career_path: '진로 예시'),
             );
           },
       child: Container(
