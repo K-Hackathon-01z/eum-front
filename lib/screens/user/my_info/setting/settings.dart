a897143d046f0149860e02fdf2a5eacadfb98ec1
whabl0220 <stargalaxy1579@gmail.com>
2025-09-28 19:05:44 +0900

    refactor: "로그아웃 팝업 디자인 변경"

--- lib/screens/user/my_info/setting/settings.dart
+++ lib/screens/user/my_info/setting/settings.dart
@@ -2,6 +2,7 @@ import 'package:eum_demo/screens/user/my_info/setting/delete_account.dart';
 import 'package:flutter/material.dart';
 import 'package:eum_demo/widgets/user/appbar.dart';
 import './change_profile.dart';
+import 'package:eum_demo/widgets/user/popup.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -50,38 +51,20 @@ class SettingsScreen extends StatelessWidget {
               trailing: IconButton(
                 icon: Icon(Icons.chevron_right),
                 onPressed: () {
-                  // 여기서 원하는 페이지로 이동
                   showDialog(
                     context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('로그아웃', textAlign: TextAlign.center),
-                        content: Text('정말 로그아웃 하시겠습니까?', textAlign: TextAlign.center),
-                        actions: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop(); // 창 닫기
-                                },
-                                child: Text('닫기'),
-                              ),
-                              SizedBox(width: 30), // 숫자를 낮추면 가까워짐 (ex. 1~2)
-                              TextButton(
-                                onPressed: () {
-                                  // 1) 모달 닫고
-                                  Navigator.of(context).pop();
-                                  // 2) 네비게이션 스택 초기화 후 홈으로 이동
-                                  Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
-                                },
-                                child: Text('로그아웃'),
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    },
+                    builder: (_) => CommonPopup(
+                      icon: Icons.logout,
+                      title: '로그아웃',
+                      message: '정말 로그아웃 하시겠습니까?',
+                      button2Text: '닫기',
+                      button1Text: '로그아웃',
+                      onButtonFirstPressed: () => Navigator.of(context).pop(),
+                      onButtonSecondPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+                      },
+                    ),
                   );
                 },
               ),
