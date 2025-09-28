51b6f5f287af763292ca56f060ac18d21a08039c
whabl0220 <stargalaxy1579@gmail.com>
2025-09-28 17:12:35 +0900

    feat: "my_info 화면에 api 회원 정보 연결"

--- lib/screens/user/my_info/my_info_real.dart
+++ lib/screens/user/my_info/my_info_real.dart
@@ -3,8 +3,9 @@ import 'package:eum_demo/screens/user/my_info/matching_requests.dart';
 import 'package:eum_demo/screens/user/my_info/setting/settings.dart';
 import 'package:eum_demo/screens/user/my_info/usage_history.dart';
 import 'package:flutter/material.dart';
-
 import '../../../services/user/my_info_service.dart';
+import 'package:provider/provider.dart';
+import '../../../providers/auth_provider.dart';
 
 class MyInfoScreen extends StatefulWidget {
   const MyInfoScreen({super.key});
@@ -17,22 +18,25 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
   String? name;
   int? age;
   String? address;
+  String? gender;
 
   @override
   void initState() {
     super.initState();
-    _fetch(); // 라이프사이클 시작 시 한 번
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _fetchUserInfo();
+    });
   }
 
-
-  //나중에 뺴야함
-  Future<void> _fetch() async {
-    final data = await MyInfoService.getUserByEmail("daniel010203@naver.com");
+  Future<void> _fetchUserInfo() async {
+    final email = Provider.of<AuthProvider>(context, listen: false).email;
+    if (email == null || email.isEmpty) return;
+    final data = await MyInfoService.getUserByEmail(email);
     setState(() {
       name = data['name'];
       address = data['address'];
-      final rawAge = data['age'];
-      age = rawAge;
+      age = data['age'];
+      gender = data['gender'];
     });
   }
 
@@ -54,10 +58,7 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
           IconButton(
             icon: Icon(Icons.settings, size: 32),
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingsScreen()),
-              );
+              Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsScreen()));
             },
           ),
           SizedBox(width: 16),
@@ -70,10 +71,7 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFF7F7F7),
-                borderRadius: BorderRadius.circular(25),
-              ),
+              decoration: BoxDecoration(color: Color(0xFFF7F7F7), borderRadius: BorderRadius.circular(25)),
               padding: EdgeInsets.all(16),
               child: Row(
                 children: [
@@ -88,29 +86,22 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
-                          // 이름,나이, 주소 , 사진등
                           children: [
-                            Text(
-                              name ?? "-",
-                              style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                            Text(name ?? "-", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
                             Spacer(),
                             Text(
                               '${age?.toString() ?? "-"}세',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.normal,
-                              ),
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
                             ),
                           ],
                         ),
                         SizedBox(height: 8),
-                        Text(
-                          address ?? "-",
-                          style: TextStyle(fontSize: 14, color: Colors.black54),
+                        Row(
+                          children: [
+                            Text(address ?? "-", style: TextStyle(fontSize: 14, color: Colors.black54)),
+                            Spacer(),
+                            Text(gender ?? "-", style: TextStyle(fontSize: 14, color: Colors.black54)),
+                          ],
                         ),
                       ],
                     ),
@@ -128,10 +119,7 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
               icon: Icon(Icons.chevron_right),
               onPressed: () {
                 // 여기서 원하는 페이지로 이동
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => UsageHistory()),
-                );
+                Navigator.push(context, MaterialPageRoute(builder: (context) => UsageHistory()));
               },
             ),
           ),
@@ -142,10 +130,7 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
               icon: Icon(Icons.chevron_right),
               onPressed: () {
                 // 여기서 원하는 페이지로 이동
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MatchingRequests()),
-                );
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MatchingRequests()));
               },
             ),
           ),
@@ -156,10 +141,7 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
               icon: Icon(Icons.chevron_right),
               onPressed: () {
                 // 여기서 원하는 페이지로 이동
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FavoriteClasses()),
-                );
+                Navigator.push(context, MaterialPageRoute(builder: (context) => FavoriteClasses()));
               },
             ),
           ),
