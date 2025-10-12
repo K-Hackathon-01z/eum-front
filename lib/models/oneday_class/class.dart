18a7a89b6fc1b5028c7eb26c52b92589a7365b2c
whabl0220 <stargalaxy1579@gmail.com>
2025-10-13 05:13:57 +0900

    refactor: "원데이 클래스 강의 분류 기준 로직 리팩토링"

--- lib/models/oneday_class/class.dart
+++ lib/models/oneday_class/class.dart
@@ -1,4 +1,49 @@
 class OnedayClass {
+  String get subCategory {
+    switch (skillId) {
+      case 1:
+        return '금속 공예';
+      case 2:
+        return '나전칠기';
+      case 3:
+        return '도자기';
+      case 4:
+        return '매듭장';
+      case 5:
+        return '칠장';
+      case 6:
+        return '한지';
+      case 7:
+        return '홍염장';
+      case 8:
+        return '한복장';
+      case 9:
+        return '자수';
+      case 10:
+        return '매듭';
+      case 11:
+        return '전통주';
+      case 12:
+        return '장';
+      case 13:
+        return '다과';
+      case 14:
+        return '판소리';
+      case 15:
+        return '민요';
+      case 16:
+        return '탈춤';
+      case 17:
+        return '무용';
+      case 18:
+        return '단청장';
+      case 19:
+        return '소목장';
+      default:
+        return '기타';
+    }
+  }
+
   final int id;
   final int skillId;
   final int artisanId;
